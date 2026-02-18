f2263a84b2913e6fe69a24b11b28fefb200dac55
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2026-02-18 18:30:16 +0700

    save recent location data

--- lib/services/recent_location_service.dart
+++ lib/services/recent_location_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tu_world_map_app/models/building.dart';
 
 class RecentLocationService {
@@ -10,19 +12,41 @@ class RecentLocationService {
 
   RecentLocationService._internal();
 
+  static const String _prefsKey = 'recent_locations';
+
   final List<Building> _recent = [];
 
-  void add(Building building) {
-    print("ADDING RECENT: ${building.name}");
+  Future<void> loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = prefs.getString(_prefsKey);
+    if (jsonString != null) {
+      final List<dynamic> jsonList = json.decode(jsonString);
+      _recent.clear();
+      _recent.addAll(jsonList.map((e) => Building.fromJson(e)).toList());
+    }
+  }
 
+  Future<void> _saveToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = json.encode(_recent.map((b) => b.toJson()).toList());
+    await prefs.setString(_prefsKey, jsonString);
+  }
+
+  void add(Building building) {
     _recent.removeWhere((b) => b.id == building.id);
     _recent.insert(0, building);
-
-    print("RECENT COUNT: ${_recent.length}");
 
     if (_recent.length > 10) {
       _recent.removeLast();
     }
+
+    _saveToPrefs();
+  }
+
+  Future<void> clear() async {
+    _recent.clear();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_prefsKey);
   }
 
   List<Building> getRecent() {
