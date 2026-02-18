9b7ec49085370ac3c868ef5808ab3f63428657a2
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2026-02-18 18:25:57 +0700

    add setting service and load recent and favorite data

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
+import 'package:tu_world_map_app/services/recent_location_service.dart';
+import 'package:tu_world_map_app/services/favorite_service.dart';
+import 'package:tu_world_map_app/services/settings_service.dart';
 import 'screens/onboarding_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((record) {
@@ -12,6 +15,12 @@ void main() {
       debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     }
   });
+
+  // Load persisted data before running the app
+  await RecentLocationService().loadFromPrefs();
+  await FavoriteService().loadFromPrefs();
+  await SettingsService().loadFromPrefs();
+
   runApp(const MyApp());
 }
 
