0cf34b63c9d7ba9c81d15ca3791f4316d85507df
6610685031 <6610685031@student.tu.ac.th>
2026-04-20 00:14:42 +0700

    adding first launch detection system

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -7,30 +7,30 @@ class SettingsService {
 
   SettingsService._internal();
 
-  static const String _locationEnabledKey = 'location_enabled';
   static const String _autoSaveRecentKey = 'auto_save_recent';
+  static const String _firstLaunchKey = 'first_launch';
 
-  bool _locationEnabled = true;
   bool _autoSaveRecent = true;
+  bool _firstLaunch = true;
 
-  bool get locationEnabled => _locationEnabled;
   bool get autoSaveRecent => _autoSaveRecent;
+  bool get firstLaunch => _firstLaunch;
 
   Future<void> loadFromPrefs() async {
     final prefs = await SharedPreferences.getInstance();
-    _locationEnabled = prefs.getBool(_locationEnabledKey) ?? true;
     _autoSaveRecent = prefs.getBool(_autoSaveRecentKey) ?? true;
-  }
-
-  Future<void> setLocationEnabled(bool value) async {
-    _locationEnabled = value;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_locationEnabledKey, value);
+    _firstLaunch = prefs.getBool(_firstLaunchKey) ?? true;
   }
 
   Future<void> setAutoSaveRecent(bool value) async {
     _autoSaveRecent = value;
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_autoSaveRecentKey, value);
+  }
+
+  Future<void> setFirstLaunchSeen() async {
+    _firstLaunch = false;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_firstLaunchKey, false);
   }
 }
